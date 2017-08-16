6871b07f07fa2391ba7b7301518d07ff99a43a1a
Josemberg Farias Ferreira <josemberg.ferreira@autotrac.com.br>
2017-08-16 16:14:46 -0300

    Ajsutes de dados cliente

--- AngularCliExemplo/HelloWorldAvancado/src/app/cliente/cliente.service.ts
+++ AngularCliExemplo/HelloWorldAvancado/src/app/cliente/cliente.service.ts
@@ -7,9 +7,10 @@ export class ClienteService {
 
   constructor() { }
 
-  listaCliente():Array<Cliente>{
-    let retorno:Array<Cliente> = new Array<Cliente>();
-    retorno.push(new Cliente({ Nome:'Cliente 1' }));
+  listaCliente(): Array<Cliente> {
+    let retorno: Array<Cliente> = new Array<Cliente>();
+    retorno.push(new Cliente({ Codigo: Math.floor((Math.random() * 100) + 1), Nome: "Cliente 1", Sobrenome: "primeiro", DataCriacao: new Date() }));
+    retorno.push(new Cliente({ Codigo: Math.floor((Math.random() * 100) + 1), Nome: "Cliente 2", Sobrenome: "segundo", DataCriacao: new Date() }));
     return retorno;
   }
 
--- AngularCliExemplo/HelloWorldAvancado/src/app/cliente/lista/lista.component.ts
+++ AngularCliExemplo/HelloWorldAvancado/src/app/cliente/lista/lista.component.ts
@@ -11,43 +11,43 @@ import * as _lodash from 'lodash';
 })
 export class ListaComponent implements OnInit {
 
-  nomeLista:string;
-  listagem:Array<Cliente>;
-  clienteAtual:Cliente;
-  mostrarFormulario:Boolean;
+  nomeLista: string;
+  listagem: Array<Cliente>;
+  clienteAtual: Cliente;
+  mostrarFormulario: Boolean;
 
-  constructor(private servicoCliente:ClienteService) { 
+  constructor(private servicoCliente: ClienteService) {
     this.nomeLista = "Lista de Clientes";
     this.mostrarFormulario = false;
 
     this.listagem = servicoCliente.listaCliente();
-
-    this.listagem.push(new Cliente({ Nome:"Cliente 2" }));
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  abrirCliente(cliente:Cliente){
+  abrirCliente(cliente: Cliente) {
     this.clienteAtual = cliente;
   }
 
-  excluirCliente(cliente:Cliente){
+  excluirCliente(cliente: Cliente) {
     // let busca = this.listagem.indexOf(cliente);
     // this.listagem.splice(busca, 1);
 
-    _lodash.remove(this.listagem, function(n) {
+    _lodash.remove(this.listagem, function (n) {
       return n == cliente;
     });
 
     this.clienteAtual = null;
   }
 
-  adicionarCliente():void{
+  adicionarCliente(): void {
     this.mostrarFormulario = !this.mostrarFormulario;
   }
 
-  cadastroCliente(cliente:Cliente):void{
+  cadastroCliente(cliente: Cliente): void {
     this.clienteAtual = null;
+    cliente.DataCriacao = new Date();
+    cliente.Codigo = Math.floor((Math.random() * 100) + 1);
     this.listagem.push(cliente);
   }
 
